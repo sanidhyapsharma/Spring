5dfa59589f81bb20ac6118637adc2c1b883a2421
Sanidhya Sharma <sanidhyaelimar@gmail.com>
2020-06-28 12:07:06 +0530

    spring boot

---
+++ src/main/java/io/codefountain/spring/todo/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package io.codefountain.spring.todo.repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import io.codefountain.spring.todo.domain.Todo;
+
+@Repository
+public interface TodoRepository extends PagingAndSortingRepository<Todo, Long>  {
+
+}
---
+++ src/test/java/io/codefountain/spring/todo/SpringBootTodoApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codefountain.spring.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
